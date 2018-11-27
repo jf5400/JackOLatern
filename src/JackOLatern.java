84eab858916c5f2454b276568f9469ff876a720a
BT_1N3_10 <jfreund5400@bths.edu>
2018-11-27 14:47:33 -0500

    things

--- src/JackOLatern.java
+++ src/JackOLatern.java
@@ -1,11 +1,31 @@
 public class JackOLatern {
+
+    private String[][] faceFeatures;
+
+    public JackOLatern(String[][] faceFeatures){
+        this.faceFeatures=faceFeatures;
+    }
+
     public void edit(String replace, int row, int column){
+        faceFeatures[row][column]=replace;
+    }
 
-    }
-    public String toString(){
-        return;
-    }
     public void fill(String str){
+        for(int i=0; i<faceFeatures.length; i++){
+            for(int j=0; j<faceFeatures[j].length; i++){
+                faceFeatures[i][j]=str;
+            }
+        }
+    }
 
+    String[][] newface=new String[][];
+    public String[][] toString() {
+        for(int i=0; i<faceFeatures.length; i++){
+            for(int j=0; j<faceFeatures[j].length; i++){
+                System.out.println(faceFeatures[i][j]);
+            }
+            System.out.println("\n");
+        }
+        return newface;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -9,16 +9,16 @@ public class Runner {
         face1.edit(" ",0, 7);
         face1.edit(" ",4, 0);
 
-        face1.edit(" ",0, 0);
-        face1.edit(" ",4, 7);
-        face1.edit(" ",0, 7);
-        face1.edit(" ",4, 0);
-        face1.edit(" ",0, 0);
-        face1.edit(" ",4, 7);
-        face1.edit(" ",0, 7);
-        face1.edit(" ",4, 0);
-        face1.edit(" ",0, 0);
-        face1.edit(" ",4, 7);
+        face1.edit("O",1, 2);
+        face1.edit("o",1, 5);
+        face1.edit("^",2, 3);
+        face1.edit("^",2, 4);
+        face1.edit("|",3, 1);
+        face1.edit("|",3, 2);
+        face1.edit("_",3, 3);
+        face1.edit("_",3, 4);
+        face1.edit("|",3, 5);
+        face1.edit("|",3, 6);
         System.out.println(face1);
     }
 }
